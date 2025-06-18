10e7d3e0f58acd9aaa701952f613fb9d2ac80ec0
Alisha Qureshi <alisha.ahmad.qureshi.28@dartmouth.edu>
2025-06-17 20:32:09 -0400

    initial commit

---
+++ CompassGuesser/CompassGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassGuesserApp.swift
+//  CompassGuesser
+//
+//  Created by Alisha Qureshi on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CompassGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
